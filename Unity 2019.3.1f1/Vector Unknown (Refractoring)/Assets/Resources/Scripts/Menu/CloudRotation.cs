72d0fc579e4ca6a5cecc068eea25fd1bc7b6839d
Jack Chen <fvjackchen@gmail.com>
2020-02-24 23:50:59 -0700

    Combine the Menu Background
    01Menu scene technically finished.

---
+++ Unity 2019.3.1f1/Vector Unknown (Refractoring)/Assets/Resources/Scripts/Menu/CloudRotation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudRotation : MonoBehaviour
+{
+    public float rotatedSpeed = 1f;
+
+    private void LateUpdate()
+    {
+        transform.Rotate(new Vector3(0, -1f, 0) * Time.deltaTime * rotatedSpeed);
+    }
+}
